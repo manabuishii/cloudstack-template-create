d1c641990a6e94df51cf32b022fa2aef2c5cbd50
Manabu ISHII <manabu.ishii.rb@gmail.com>
2015-03-18 13:27:36 +0900

    truncate .bash_history

--- ubuntu1404/ubuntu-remove-chef.sh
+++ ubuntu1404/ubuntu-remove-chef.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# RECOMMENDED
+# export HISTSIZE=0
+# run this script on /tmp
 apt-get -y remove --purge chef
 rm -rf /etc/chef
 rm -rf /var/chef
 rm -rf /opt/chef
+rm /root/.ssh/authorized_keys
 history -c
-history -w
+truncate -s 0 /root/.bash_history
 shutdown -h now
